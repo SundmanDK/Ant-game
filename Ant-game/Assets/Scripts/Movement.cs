07bb5b8feb94c40d1ba2ea0ec3485a5c8425d938
Askebar <askeand@ruc.dk>
2022-03-17 13:05:33 +0100

    Myre be like haha im a circle when it eats food
    den skifter også retning når den samler mad op. Den kan holde på mad og altsådan noget.

--- Ant-game/Assets/Scripts/Movement.cs
+++ Ant-game/Assets/Scripts/Movement.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Movement: MonoBehaviour{
     private Rigidbody2D rigidbodyComponent;
     public float moveSpeed = 6;
+    public GameObject food;
+    private Rigidbody2D rigidbodyComponentChild;
+    public bool holdingFood;
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
 
     // Start is called before the first frame update
     void Start(){
         rigidbodyComponent = GetComponent<Rigidbody2D>();
+        holdingFood = false;
+    }
+
+    void ChangeSprite(){
+        spriteRenderer.sprite = newSprite;
     }
 
     // Update is called once per frame
@@ -17,7 +27,7 @@ public class Movement: MonoBehaviour{
     }
     // Update is called once per phycis update
     void FixedUpdate(){
-        rigidbodyComponent.velocity = transform.up * moveSpeed;
+        rigidbodyComponent.velocity = transform.up * moveSpeed;        
         int chooseMove = Random.Range(1, 4);
         if (chooseMove == 1) {
             transform.RotateAround(transform.position, transform.forward, -10f);
@@ -31,8 +41,12 @@ public class Movement: MonoBehaviour{
 
     }
     private void OnTriggerEnter2D(Collider2D col){
-        if (col.gameObject.layer == 6){
+        if (col.gameObject.layer == 6 && holdingFood == false){
             Destroy(col.gameObject);
+            holdingFood = true;
+            Debug.Log("mr. ant is holding food");
+            ChangeSprite();
+            transform.RotateAround(transform.position, transform.forward, 180f);
         }
     }
 
