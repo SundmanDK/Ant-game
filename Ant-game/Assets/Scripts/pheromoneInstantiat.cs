48bf44cb0cd3612f68d9b7fe71cfdc98362c2f76
Sebastianrix <sebastianrix11@gmail.com>
2022-03-18 13:31:03 +0100

    Pheromones early stage

---
+++ Ant-game/Assets/Scripts/pheromoneInstantiat.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class pheromoneInstantiat : MonoBehaviour
+{
+    public float timeToThink = 2.0f;
+    public float timer = 0.0f;
+
+    public Transform prefab;
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > timeToThink)
+        {
+
+            Instantiate(prefab, transform.position, Quaternion.identity);
+            timer = timer - timeToThink;
+            //Debug.Log();
+        }
+    }
+}
