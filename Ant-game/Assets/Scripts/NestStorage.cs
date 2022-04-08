aec20be93c58d1b419f3618a1ac48b1415cd7cff
SundmanDK <72975724+SundmanDK@users.noreply.github.com>
2022-04-08 12:17:37 +0200

    Centered scene

--- Ant-game/Assets/Scripts/NestStorage.cs
+++ Ant-game/Assets/Scripts/NestStorage.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 public class NestStorage : MonoBehaviour{
     public int food;
 
-
     // Start is called before the first frame update
     void Start() {
         food = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
