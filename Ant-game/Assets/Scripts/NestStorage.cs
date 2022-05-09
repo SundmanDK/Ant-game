15b7817a85fd3ea9e840646c2acb6647290408b1
Sebastianrix <sebastianrix11@gmail.com>
2022-05-09 13:24:18 +0200

    Concept art for anthill

--- Ant-game/Assets/Scripts/NestStorage.cs
+++ Ant-game/Assets/Scripts/NestStorage.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class NestStorage : MonoBehaviour{
     public int food;
+    public int score;
+    public int gold;
 
     // Start is called before the first frame update
     void Start() {
-        food = 0;
+        food  = 0;
+        score = 0;
+        gold  = 0;
+
+
+        Debug.Log(food);
+
     }
 }
