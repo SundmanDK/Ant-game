21962082343651b3877af927f28b778f43db7042
SundmanDK <72975724+SundmanDK@users.noreply.github.com>
2022-05-10 13:06:47 +0200

    clean up

--- Ant-game/Assets/Scripts/StatSheet.cs
+++ Ant-game/Assets/Scripts/StatSheet.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatSheet : MonoBehaviour
-{
+public class StatSheet : MonoBehaviour{
+
+    public float attackSpeed;
+    public float damage;
+    public float health;
+    public float armor;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
