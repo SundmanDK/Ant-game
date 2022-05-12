6038319c620dbd4b19762ce6e9415920e59d67d7
Sebastianrix <sebastianrix11@gmail.com>
2022-05-12 14:57:26 +0200

    ToolTips. Require LeanTween

---
+++ Ant-game/Assets/Scripts/TooltipTrigger.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private static LTDescr delay;
+    public string content;
+    public string header;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        delay = LeanTween.delayedCall(0.5f, () =>
+        {
+            TooltipSystem.Show(content, header);
+        });
+        
+        
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.cancel(delay.uniqueId);
+        TooltipSystem.Hide();
+    }
+}
