9dfae347c97045127a11364a5525851d11957d6b
Rasmusbeyer <91940353+Rasmusbeyer@users.noreply.github.com>
2022-05-18 13:15:22 +0200

    wasp charge ish
    yes but no

--- Ant-game/Assets/Scripts/WaspStats.cs
+++ Ant-game/Assets/Scripts/WaspStats.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaspStats : Stats{
+    private float chargeCooldown = 10;
+    public bool chargeOnCooldown = false;
+    private float timeOnCooldown = 0;
+    private SpiderStuff behaviour;
+    private float timeRemaining = 2;
+    private float startSpeed;
+    private bool endCharge;
 
-    float chargeTimer = 0; 
-
-    //void FixedUpdate(){ }
-
-    public override void TakeDamage(int Dmg){
-        base.TakeDamage(Dmg);
+    void Start(){
+        behaviour = GetComponent<SpiderStuff>();
+        startSpeed = moveSpeed;
     }
 
+    void Update(){
+        if(behaviour.followingAnt){
+            Charge();
+        }
+        if(chargeOnCooldown){
+            timeOnCooldown += Time.deltaTime;
+            if(timeRemaining > timeOnCooldown && endCharge == false){
+                ChargeAction();
+                }
+            if (timeOnCooldown > chargeCooldown){
+                chargeOnCooldown = false;
+                timeOnCooldown = 0;
+            }
+        }
+    }
+    private void ChargeAction(){
+        moveSpeed = 30;
+    }
+    private void Charge(){
+        moveSpeed = startSpeed;
+        chargeOnCooldown = true;
+
+    }
+    protected void Attack(Collision2D target){
+        base.Attack(target);
+        moveSpeed = startSpeed;
+        endCharge = true;
+     
+    }
+        void FixedUpdate(){
+        AttackTimer();
+        if(health <= 0){
+            Death();
+        }
+    }
 }
+
